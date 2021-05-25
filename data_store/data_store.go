d52c0fa0235ffd0e6d8bb0268405be8b6f38b8fb
Ariel <apchenet@gmail.com>
2021-05-25 22:06:29 +0200

    Adding in start task command
    - Rethink data structures used?

--- data_store/data_store.go
+++ data_store/data_store.go
@@ -1,10 +1,33 @@
 // The data_store package store data on the different tasks.
+// Store information in a FOLDER, with each file being a task?
 package data_store
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+	"fmt"
+)
+
+type DataStore struct {
+	TaskList
+	Storage string
+}
+
+func NewDataStore(taskList TaskList, storage string) *DataStore {
+	return &DataStore{
+		taskList,
+		storage,
+	}
+}
 
 type TaskList map[string]Task
 
 type Task struct {
-	Name string // Should always be the same as the key for this task in the map.
+	Name         string // Should always be the same as the key for this task in the map.
+	TotalTime    time.Duration
+	CurrentStart time.Time
+	Finished     bool
 }
 
 func (tl TaskList) CheckThatAllNamesAreCorrect() bool {
@@ -14,4 +37,19 @@ func (tl TaskList) CheckThatAllNamesAreCorrect() bool {
 		}
 	}
 	return true
+}
+
+
+func WriteToFile(file *os.File, data interface{}) error {
+	// Marshal to json
+	marshaledData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(marshaledData)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Wrote to file", file)
+	return nil
 }
--- main.go
+++ main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"fmt"
-	"github.com/achenet/TimeLog/data_store"
+	"github.com/achenet/TimeLog/cmd"
+	"log"
+	"os"
 )
 
-func main() {
-	fmt.Println("TimeLog")
-	fmt.Println(FirstBranchTest())
+const (
+	TaskListDirectory = "~/.taskList/"
+)
+
+var (
+	tl cmd.TimeLogger
+	taskList data_store.TaskList
+)
+
+func init() {
+	// Make storage directory if it isn't already created
+	if err := os.MkdirAll(TaskListDirectory, 744); err != nil {
+		log.Fatal("Error making storage directory:", err.Error())
+	}
+
+	// Read from storage directory if it has anything in it. 
+	storageDirectory, err := os.Open(TaskListDirectory)
+	if err != nil {
+		log.Fatal("Error opening storage directory:", err.Error())
+	}
+	storageDirectoryEntries, err := storageDirectory.ReadDir(-1)
+	if err != nil {
+		log.Fatal("Error reading storage directory", err.Error())
+	}
+
+	// If empty, finish init
+	if len(storageDirectoryEntries) == 0 {
+		return
+	}
+
+	for _, task := range storageDirectoryEntries {
+
+	}
 }
 
-func FirstBranchTest() (tl data_store.TaskList) {
-	tl = make(data_store.TaskList)
-	tl["first"] = data_store.Task{
-		Name: "first",
-	}
-	return tl
+func main() {
+	tl = NewTimeLogger(taskList)
+	fmt.Println("TimeLog")
+	tl.Execute()
+}
+
+func readTaskFromFile(fileName string) error {
+
+	return nil
 }
