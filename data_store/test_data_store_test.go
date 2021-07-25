18097ff4b348ccc6640dca80fe8fe2c933cd6129
Ariel <apchenet@gmail.com>
2021-07-25 17:08:06 +0200

    Delete old tests.

--- data_store/test_data_store_test.go
+++ data_store/test_data_store_test.go
@@ -12,30 +12,8 @@ var testDataStore *DataStore
 
 func TestMain(m *testing.M) {
 	fmt.Println("Testing data store module")
-	var taskList TaskList
 	var storage = testStorage
-	testDataStore = NewDataStore(taskList, storage)
 	m.Run()
 }
 
-func TestWriteToFile(t *testing.T) {
-	fmt.Println("Testing WriteToFile")
-	testToWrite := TaskList{
-		"snoop": {
-			Name: "snoop",
-		},
-		"jack": {
-			Name: "jack",
-		},
-	}
-	testDataStore.TaskList = testToWrite
-	file, err := os.Create(testStorage)
-	if err != nil {
-		t.Error(err.Error())
-	}
-	testDataStore.WriteToFile(file, testToWrite)
-}
-
-func TestReadFromFile(t *testing.T) {
-	// Would first have to write test file
-}
+// Refactored code, so tests will have to be changed.
